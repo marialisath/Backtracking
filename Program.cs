83532af22a9e6aa80e44ae22ad3c70ba00faa1eb
Maria Lisa <mlt1@nemandi.ntv.is>
2022-03-06 21:56:48 +0000

    Start

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace backtracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> board = new List<List<int>> {
+            new List<int> { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+            new List<int> { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+            new List<int> { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+            new List<int> { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+            new List<int> { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+            new List<int> { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+            new List<int> { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+            new List<int> { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+            new List<int> { 0, 0, 0, 0, 8, 0, 0, 7, 9 },
+            };
+
+        }
+    }
+}
